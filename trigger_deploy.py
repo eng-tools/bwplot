abcdf0ac9cd2b64c252d588ba427afb1c9def2df
millen1m <maxim.millen@gmail.com>
2018-04-05 18:37:45 +0100

    can pack kwargs

--- trigger_deploy.py
+++ trigger_deploy.py
@@ -1,4 +1,6 @@
 import subprocess
+import py
+
 
 about = {}
 with open("bwplot/__about__.py") as fp:
@@ -7,8 +9,10 @@ with open("bwplot/__about__.py") as fp:
 
 version = about['__version__']
 
-subprocess.check_call(["git", "tag", version, "-m", "version %s" % version])
-subprocess.check_call(["git", "push", "--tags", "origin", "pypi"])
+failures = py.test.cmdline.main()
+if failures == 0:
+    subprocess.check_call(["git", "tag", version, "-m", "version %s" % version])
+    subprocess.check_call(["git", "push", "--tags", "origin", "pypi"])
 
 # git push --tags origin pypi
 # git tag 0.5.2 -m "version 0.5.2"
